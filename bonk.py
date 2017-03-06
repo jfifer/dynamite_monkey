a8700e0af50903f5855c3a5b4d5474f19f3eaf95
Joe Fifer <jfifer@coredial.com>
2017-03-06 16:16:28 -0500

    Polished up

--- bonk.py
+++ bonk.py
@@ -14,24 +14,30 @@ class gateway (threading.Thread):
     self.dbUser = dbUser
     self.dbPass = dbPass
     self.gw = mysql.connector.connect(user=dbUser, password=dbPass, host=dbHost, database=gw_name)
+
   def run(self):
-    print "Populating from %s..." % (self.gw_name)
+    print "Gathering CDR Data from %s..." % (self.gw_name)
     cursor = self.gw.cursor()
-    sql = "SELECT * FROM cdr WHERE callDate BETWEEN DATE_SUB(NOW(), INTERVAL 2 MONTH) AND NOW()"
+    sql = "SELECT * FROM cdr WHERE callDate > '%s' AND id > %d" % (self.last_completed, self.last_max_id)
+    #sql = "SELECT * FROM cdr WHERE callDate > DATE_SUB(NOW(), INTERVAL 2 MONTH)";
     cursor.execute(sql)
     result = cursor.fetchall()
     self.gw.close()
-    print "Inserting [%s]..." % (self.gw_name)
+    print "Populating %s.local_cdr..." % (self.gw_name)
     billing = mysql.connector.connect(user='root', password='monkeyshit',
                               host='127.0.0.1',
                               database=self.gw_name)
     for row in result:
       bCursor = billing.cursor()
       ins = "INSERT INTO local_cdr VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-
       bCursor.execute(ins, (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19]))
+
+    self.gw = mysql.connector.connect(user="root", password="monkeyshit", host="localhost", database="dynamite_monkey")
+    cursor = self.gw.cursor()
+    sql = "UPDATE dynamite_monkey.gw_ids_xref SET last_max_id=(SELECT MAX(id) FROM %s.local_cdr), last_completed=NOW() WHERE gw_name='%s'" % (self.gw_name, self.gw_name)
+    cursor.execute(sql);
     billing.close()
-    
+    print "%s Complete." % (self.gw_name)
 
 def main(argv):
   cnx = mysql.connector.connect(user='root', password='monkeyshit',
@@ -58,8 +64,6 @@ def main(argv):
       t.start()
   except:
     print "Error: Unable to fetch Gateway Data"
-
-  #cnx.close()
 
 if __name__ == "__main__":
   main(sys.argv[1:])
